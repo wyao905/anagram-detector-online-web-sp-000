d2be13e5d11c4f81cf3c7c5504895243bddd4a27
Warren Yao <warrenyca905@gmail.com>
2019-06-02 19:31:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :word
-  
   def initialize(word)
     @word = word
   end
   
   def match(some_words)
+    init_word = @word.split("").sort
+    some_words.each
   end
 end
